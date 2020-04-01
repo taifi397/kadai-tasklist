bc82793c20e3364d191da044ae2decfa4bce7a8d
taifi.guenre <guenretaifi@gmail.com>
2020-04-01 13:47:26 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,8 +7,14 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('title', 'ステータス:') !!}
+                    <input type="text" name="status"/>
+                </div>
         
                 <div class="form-group">
+                    
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,6 +7,11 @@
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('title', 'ステータス:') !!}
+                    <input type="text" name="status"/>
+                </div>
         
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
