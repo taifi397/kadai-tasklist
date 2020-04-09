1d0894175546f1efddf8865df9982c4512c1f805
taifi.guenre <guenretaifi@gmail.com>
2020-04-09 08:52:35 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,3 +93,14 @@
         </div>
     </body>
 </html>
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Microposts</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endsection
