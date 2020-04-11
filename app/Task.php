09485a890a613a72ed5cafe93cf55fc0ee4be528
taifi.guenre <guenretaifi@gmail.com>
2020-04-11 12:44:39 +0000

    post

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['content', 'user_id', 'status'];
+    protected $fillable = ['content','tasks', 'status'];
 
     public function user()
     {
